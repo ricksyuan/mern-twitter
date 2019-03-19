6dd389c08b376f9ff22508865e8da9e32395718a
Rick Yuan <rick.s.yuan@gmail.com>
2019-03-19 15:01:26 -0400

    Create routes for users and tweets

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+const users = require('./routes/api/users');
+const tweets = require('./routes/api/tweets');
+
+app.use('/api/users', users);
+app.use('/api/tweets', tweets);
 
 mongoose
   .connect(db, { useNewUrlParser: true })
